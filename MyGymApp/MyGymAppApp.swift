cd6c997156879ed6106e935fcb27ca81229a88c5
Dillon <s0299646@student.rockvalleycollege.edu>
2023-10-25 19:31:17 -0500

    Initial Commit

---
+++ MyGymApp/MyGymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGymAppApp.swift
+//  MyGymApp
+//
+//  Created by Dillon Brown on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MyGymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
