6fadf10f96875b4ace5bcf3f880301eb239d6f70
Dillon <s0299646@student.rockvalleycollege.edu>
2023-11-08 19:17:46 -0600

    Posting Wireframe

--- MyGymApp/ContentView.swift
+++ MyGymApp/ContentView.swift
@@ -1,24 +1,38 @@
-//
-//  ContentView.swift
-//  MyGymApp
-//
-//  Created by Dillon Brown on 10/25/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MyNavigationStack()
         }
-        .padding()
+    }
+}
+struct MyNavigationStack: View {
+    var body: some View {
+        Text("Fitness Calculators")
+        NavigationStack {
+            List {
+                NavigationLink {
+                    Text("One Rep Max Calculator")
+                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                } label: {
+                    Label("One Rep Max Calculator", systemImage: "link")
+                }
+                NavigationLink {
+                    Text("BMI Calculator")
+                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                } label: {
+                    Label("BMI Calculator", systemImage: "link")
+                }
+                NavigationLink {
+                    Text("Recommended Protein Calculator")
+                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                } label: {
+                    Label("Recommended Protein Calculator", systemImage: "link")
+                }
+                
+            }
+        }
     }
 }
 
-#Preview {
-    ContentView()
-}
